fe070f4ebd95bdc8428fb8f103c24a2875a575b1
Hottory <gkffndnl123@naver.com>
2018-12-30 14:18:32 +0900

    회원가입 json 파싱 시작

--- app/src/main/java/kr/tjeit/serverapitest/SignUpActivity.java
+++ app/src/main/java/kr/tjeit/serverapitest/SignUpActivity.java
@@ -1,10 +1,25 @@
 package kr.tjeit.serverapitest;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import kr.tjeit.serverapitest.utils.PasswordUtil;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class SignUpActivity extends BaseActivity {
 
@@ -69,6 +84,62 @@ public class SignUpActivity extends BaseActivity {
                      return;
 
                 }
+
+                OkHttpClient client = new OkHttpClient();
+                RequestBody requestBody = new FormBody.Builder()
+                        .add("user_id",userIdEdt.getText().toString())
+                        .add("password",PasswordUtil.getEncrytedPassword(userPwEdt.getText().toString()))
+                        .add("name", userNameEdt.getText().toString())
+                        .add("phone", userPhoneEdt.getText().toString())
+                        .add("email", userEmailEdt.getText().toString())
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url("http://api-dev.lebit.kr/auth")
+                        .put(requestBody)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+
+                                Toast.makeText(mContext, "서버통신 실패", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+
+
+
+
+                        Log.d("회원가입리스폰스",response.body().string());
+                        try {
+
+
+                            JSONObject root = new JSONObject(response.body().string());
+
+                            int code = root.getInt("code");
+                            String message = root.getString("message");
+                            Log.d("회원가입 리스폰스" ,"코드"+code);
+                            Log.d("회원가입 리스폰스" ,"메세지"+code);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
+
+
+
+
 
 
 
