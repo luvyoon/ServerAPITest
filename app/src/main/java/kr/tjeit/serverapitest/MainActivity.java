5745c291805f3b8749fc8ed532940422ce49227f
Hottory <gkffndnl123@naver.com>
2018-12-30 16:22:12 +0900

    게시판 목록 가져오기 시도중

--- app/src/main/java/kr/tjeit/serverapitest/MainActivity.java
+++ app/src/main/java/kr/tjeit/serverapitest/MainActivity.java
@@ -1,15 +1,34 @@
 package kr.tjeit.serverapitest;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+import kr.tjeit.serverapitest.datas.Board;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends BaseActivity {
 
-    private android.widget.TextView userNameTxt
-;
+    String token;
+
+    List<Board> boards = new ArrayList<Board>();
+
+    private android.widget.TextView userNameTxt;
     private android.widget.TextView userIdTxt;
     private android.widget.TextView userEmailTxt;
     private android.widget.TextView userPhoneTxt;
+    private android.widget.ListView boardListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +38,7 @@ public class MainActivity extends BaseActivity {
 
         bindViews();
         setEvents();
-        setEvents();
+        setValues();
     }
 
     @Override
@@ -27,17 +46,61 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    void getBoardsFromServer() {
+
+        OkHttpClient client = new OkHttpClient();
+
+//        밑의 두줄이 GET 만의 방식. post/put 과의 차이점점
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("http://api-dev.lebit.kr/board").newBuilder();
+
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .header("X-Http-Token", "토큰값")//헤더가 필요한 경우에만 작성
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String responseBody = response.body().string();
+                Log.d("게시판조회",responseBody);
+
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getBoardsFromServer();
+    }
+
+
+
     @Override
     public void setValues() {
 
-        String loginid = getIntent().getStringExtra("로그인아이디");
+        token = getIntent().getStringExtra("토큰");
+
+        String loginId = getIntent().getStringExtra("로그인아이디");
         String loginEmail = getIntent().getStringExtra("이메일");
         String loginName = getIntent().getStringExtra("이름");
         String loginPhone = getIntent().getStringExtra("폰번");
 
-        userNameTxt.setText(loginid);
-        userEmailTxt.setText(loginEmail);
         userNameTxt.setText(loginName);
+        userIdTxt.setText(loginId);
+        userEmailTxt.setText(loginEmail);
         userPhoneTxt.setText(loginPhone);
 
 
@@ -45,11 +108,12 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
         this.userPhoneTxt = (TextView) findViewById(R.id.userPhoneTxt);
         this.userEmailTxt = (TextView) findViewById(R.id.userEmailTxt);
         this.userIdTxt = (TextView) findViewById(R.id.userIdTxt);
         this.userNameTxt = (TextView) findViewById(R.id.userNameTxt);
+        this.boardListView = (ListView) findViewById(R.id.boardListView);
+
 
     }
 }
